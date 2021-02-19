13a161efa73b65eae939f614d0e57b05c43552a5
Scott Bass <scottbass47@gmail.com>
2021-02-19 15:11:09 -0500

    Remove test code

--- src/main/java/mtm68/util/StringUtils.java
+++ src/main/java/mtm68/util/StringUtils.java
@@ -11,15 +11,4 @@ public class StringUtils {
 		String digits = hex.substring(2); // chop off \x
 		return Long.parseLong(digits, 16);
 	}
-	
-	public static void main(String[] args) {
-		
-		char result = convertHexToChar("\\x0041");
-		
-		String sub = "'\\x0041'".replace("'", "");
-		
-		System.out.println("Result: " + result);
-		System.out.println("Test: " + (long)'x');
-		System.out.println("Long: " + convertHexToLong(sub));
-	}
 }
