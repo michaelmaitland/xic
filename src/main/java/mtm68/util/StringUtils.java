ebaa729f589c14a9d02d247fd03b5e2f2b63a7fd
Scott Bass <scottbass47@gmail.com>
2021-02-22 14:56:50 -0500

    Removed junk code

--- src/main/java/mtm68/util/StringUtils.java
+++ src/main/java/mtm68/util/StringUtils.java
@@ -1,7 +1,5 @@
 package mtm68.util;
 
-import java.nio.file.Files;
-
 public class StringUtils {
 	
 	/**
@@ -26,13 +24,5 @@ public class StringUtils {
 	public static long convertHexToLong(String hex) {
 		String digits = hex.substring(2); // chop off \x
 		return Long.parseLong(digits, 16);
-	}
-
-	public static void main(String[] args) {
-		String filename = "hello_world.xi";
-		String test = "hello world!\n";
-		
-		String replaced = filename.replaceFirst("\\.[0-9a-zA-Z]+$", ".lexed");
-		System.out.println(test.replaceAll("[\n]", "\\\\n"));
 	}
 }
