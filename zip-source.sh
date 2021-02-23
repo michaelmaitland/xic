72fe28630671963ba228209ff96cd116b54210db
Michael Maitland <michaelmaitland@verizon.net>
2021-02-22 23:45:10 -0500

    finish packaging offline

--- zip-source.sh
+++ zip-source.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+# Dependencies
+mvn install:install-file \
+   -Dfile=${ROOT_DIR}/lib/jflex-1.8.2/lib/jflex-full-1.8.2.jar \
+   -DgroupId=de.jflex \
+   -DartifactId=jflex \
+   -Dversion=1.8.2 \
+   -Dpackaging=jar \
+   -DgeneratePom=true
+# Create jar
+mvn package -Dmaven.repo.local=${ROOT_DIR}/lib package
+
 zip -r source-$(uuidgen).zip lib src pom.xml xic-build
