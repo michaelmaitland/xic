e658f5d8a0f96ec92bdedb4cba90b7a54fcb09f9
Mike Tobin <tobin9299@gmail.com>
2021-03-08 14:09:32 -0500

    Added ast to .parsed file output

--- src/main/java/mtm68/Main.java
+++ src/main/java/mtm68/Main.java
@@ -2,7 +2,6 @@ package mtm68;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -21,7 +20,6 @@ import org.kohsuke.args4j.ParserProperties;
 import edu.cornell.cs.cs4120.util.CodeWriterSExpPrinter;
 import edu.cornell.cs.cs4120.util.SExpPrinter;
 import java_cup.runtime.ComplexSymbolFactory;
-import mtm68.ast.nodes.Expr;
 import mtm68.ast.nodes.Node;
 import mtm68.lexer.Lexer;
 import mtm68.lexer.FileTypeLexer;
@@ -85,27 +83,29 @@ public class Main {
 			}
 			Lexer lexx = new FileTypeLexer(new FileReader(filename), FileType.parseFileType(filename));
 			Parser parser = new Parser(lexx, new ComplexSymbolFactory());
-			Node parsed = (Node)(parser.parse().value);
-			SExpPrinter printer = new CodeWriterSExpPrinter(new PrintWriter(System.out));
-			parsed.prettyPrint(printer);
-			printer.flush();
-			System.out.println("Result: " + parsed);
-
-			SourceFileLexer lexer = new SourceFileLexer(filename, sourcePath);
-			List<Token> tokens = lexer.getTokens();
+			Node ast = (Node)(parser.parse().value);
+			
+			//Persist
 			if (lex) {
+				SourceFileLexer lexer = new SourceFileLexer(filename, sourcePath);
+				List<Token> tokens = lexer.getTokens();	
 				writeToFile(filename, tokens);
 			}
+			if(parse) writeToFile(filename, ast);
 			
+			SExpPrinter printer = new CodeWriterSExpPrinter(new PrintWriter(System.out));
+			ast.prettyPrint(printer);
+			printer.flush();
+			System.out.println("Result: " + ast);
+
+
 			
-			// Node ast = parse(tokens);
-			// if (parse) writeToFile(filename, ast);
 		}
 	}
 
 	/**
 	 * Writes lexed results to [filename.lexed] 
-	 * Requires: filename is of the form filename.xi or filename.ix
+	 * Requires: filename is of the form filename.xi or filename.ixi
 	 * 
 	 * @param filename the name of the file lexed
 	 * @param tokens   the list of lexed tokens
@@ -120,6 +120,26 @@ public class Main {
 					.collect(Collectors.toList()), Charset.defaultCharset());
 		} catch (IOException e) {
 			System.out.println("Failed writing lexer results to " + dPath.resolve(outfile) + " for " + filename);
+		}
+	}
+	
+	/**
+	 * Writes parsed AST to [filename.parsed] 
+	 * Requires: filename is of the form filename.xi or filename.ixi
+	 * 
+	 * @param filename the name of the file parsed
+	 * @param ast      the root node of the ast
+	 */
+	public void writeToFile(String filename, Node ast) {
+		String outfile = filename.replaceFirst("\\.(xi|ixi)", ".parsed");
+		Path outpath = dPath.resolve(outfile);
+		try {
+			Files.createDirectories(outpath.getParent());
+			SExpPrinter printer = new CodeWriterSExpPrinter(new PrintWriter(outpath.toFile()));
+			ast.prettyPrint(printer);
+			printer.close();
+		} catch (IOException e) {
+			System.out.println("Failed writing parser results to " + dPath.resolve(outfile) + " for " + filename);
 		}
 	}
 
