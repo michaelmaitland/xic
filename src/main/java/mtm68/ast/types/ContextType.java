d3c3cff95fc116025c0ea6c3f27b6021e356a8a7
Mike Tobin <tobin9299@gmail.com>
2021-03-20 12:40:05 -0400

    Added generated equals to ContextType

--- src/main/java/mtm68/ast/types/ContextType.java
+++ src/main/java/mtm68/ast/types/ContextType.java
@@ -44,5 +44,46 @@ public class ContextType {
 	public boolean isFunctionDecl() {
 		return isFunctionDecl;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((argTypes == null) ? 0 : argTypes.hashCode());
+		result = prime * result + (isFunctionDecl ? 1231 : 1237);
+		result = prime * result
+				+ ((returnTypes == null) ? 0 : returnTypes.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ContextType other = (ContextType) obj;
+		if (argTypes == null) {
+			if (other.argTypes != null)
+				return false;
+		} else if (!argTypes.equals(other.argTypes))
+			return false;
+		if (isFunctionDecl != other.isFunctionDecl)
+			return false;
+		if (returnTypes == null) {
+			if (other.returnTypes != null)
+				return false;
+		} else if (!returnTypes.equals(other.returnTypes))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
 }
