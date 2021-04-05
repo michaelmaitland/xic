b7f3b63e2939c6037c26ed2b1d174380411ee155
Michael Maitland <michaelmaitland@verizon.net>
2021-04-05 15:34:28 -0400

    documentation

--- src/main/java/mtm68/ast/nodes/Expr.java
+++ src/main/java/mtm68/ast/nodes/Expr.java
@@ -4,7 +4,7 @@ import edu.cornell.cs.cs4120.ir.IRExpr;
 import mtm68.ast.types.HasType;
 import mtm68.ast.types.Type;
 
-public abstract class Expr extends Node implements  HasType {
+public abstract class Expr extends Node implements HasType {
 
 	protected Type type;
 	
@@ -26,13 +26,24 @@ public abstract class Expr extends Node implements  HasType {
 	public void setIRExpr(IRExpr irExpr) {
 		this.irExpr = irExpr;
 	}
-
+	/**
+	 * Copies this Expr, sets the type of the copied Expr,
+	 *  and returns that copied Expr.
+	 * @param type the type to set the copied Expr
+	 * @return the copied Expr
+	 */
 	public <E extends Expr> E copyAndSetType(Type type) {
 		E newE = this.copy();
 		newE.setType(type);
 		return newE;
 	}
 	
+	/**
+	 * Copies this Expr, sets the IRExpr of the copied Expr,
+	 *  and returns that copied Expr.
+	 * @param expr the IRExpr to set the copied Expr
+	 * @return the copied Expr
+	 */
 	public <E extends Expr> E copyAndSetIRExpr(IRExpr expr) {
 		E newE = this.copy();
 		newE.setIRExpr(expr);
